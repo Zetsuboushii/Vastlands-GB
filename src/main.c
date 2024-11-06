e19f655f69537737c4c7be33c9b8cb717435115a
Zetsuboushii <zetsuboushii@gmail.com>
2024-11-06 17:47:37 +0100

    feat: added sprite and rudimentary isometric movement

--- src/main.c
+++ src/main.c
@@ -5,8 +5,10 @@
 #include "../res/tiles.c"
 #include "player.h"
 
-void main()
-{
+uint8_t joypadCurrent = 0, joypadPrevious = 0;
+uint8_t playerX = 72, playerY = 92;
+
+void main() {
     SHOW_BKG;
     SHOW_SPRITES;
     DISPLAY_ON;
@@ -14,11 +16,28 @@ void main()
     set_bkg_data(0, 58, tileTiles);
     set_bkg_tiles(0, 0, 20, 18, tileMap);
 
-    //set_sprite_data(0, player_TILE_COUNT, player_tiles);
-    //move_metasprite_ex(player_metasprites, 0, 0, 0, 80, 80);
+    set_sprite_data(0, 4, player_tiles);
 
-    while (1)
-    {
+    while (1) {
+        joypadPrevious = joypadCurrent;
+        joypadCurrent = joypad();
+
+        if ((joypadCurrent & J_LEFT) && !(joypadPrevious & J_LEFT)) {
+            playerX -= 16;
+            playerY -= 8;
+        } else if ((joypadCurrent & J_RIGHT) && !(joypadPrevious & J_RIGHT)) {
+            playerX += 16;
+            playerY += 8;
+        } else if ((joypadCurrent & J_DOWN) && !(joypadPrevious & J_DOWN)) {
+            playerX -= 16;
+            playerY += 8;
+        } else if ((joypadCurrent & J_UP) && !(joypadPrevious & J_UP)) {
+            playerX += 16;
+            playerY -= 8;
+        }
+
+        move_metasprite_ex(player_metasprites[0], 0, 0, 0, playerX, playerY);
+
         wait_vbl_done();
     }
 }
--- src/player.c
+++ src/player.c
@@ -29,9 +29,9 @@ const uint8_t player_tiles[64] = {
 
 const metasprite_t player_metasprite0[] = {
 	{.dy=-8, .dx=-8, .dtile=0, .props=0},
-	{.dy=0, .dx=8, .dtile=1, .props=0},
-	{.dy=8, .dx=-8, .dtile=2, .props=0},
-	{.dy=0, .dx=8, .dtile=3, .props=0},
+	{.dy=8, .dx=0, .dtile=1, .props=0},
+	{.dy=-8, .dx=8, .dtile=2, .props=0},
+	{.dy=8, .dx=0, .dtile=3, .props=0},
 	METASPR_TERM
 };
 
